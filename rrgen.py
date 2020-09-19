11859a4c2bb08fdcb5eee06fc00b3c8b1976fe70
Thiago de Sousa <sthiago@gmail.com>
2020-09-19 14:24:36 -0300

    Add comments for configuration sections

--- rrgen.py
+++ rrgen.py
@@ -11,6 +11,7 @@ from draw import Drawer
 args = parser.parse_args()
 print(args)
 
+# Show available colors
 if args.colors:
     longest = max(list(Colors), key=lambda c: len(c.name))
     padding = len(longest.name) + 1
@@ -19,6 +20,7 @@ if args.colors:
         print(color.name.rjust(padding), color.value)
     exit(0)
 
+# Configure map color
 if args.color:
     try:
         color = Colors[args.color]
@@ -28,6 +30,7 @@ if args.color:
 else:
     color = random.choice(list(Colors))
 
+# Configure start position
 if args.start_at is None:
     start_options = {
         'bottom_left' : Node(0, 0),
@@ -43,6 +46,7 @@ else:
             '0 <= X < WIDTH and 0 <= Y < HEIGHT')
     start_position = Node(x, y)
 
+# Configure random seed
 if args.seed is None:
     seed = random.randrange(sys.maxsize)
     random.seed(seed)
@@ -53,6 +57,7 @@ else:
 if args.show_seed:
     print("Seed:", seed)
 
+# Start generation
 grid = Grid(args.width, args.height)
 drawer = Drawer(grid, args.cell_size, args.wall_thickness, args.padding,
     args.hide_arrows, args.hide_start, args.hide_finish, args.hide_github)
@@ -64,10 +69,10 @@ if args.show_path:
     path_str = str(path)
     print("Path:", path_str)
 
+# Draw image to file
 drawer.draw_path(path, 1)
 img = drawer.img
 img.reverse()
-
 palette = [ (0, 0, 0, 255), color.value ]
 w = png.Writer(len(img[0]), len(img), palette=palette, bitdepth=8)
 f = open(os.path.expanduser(args.output), 'wb')
